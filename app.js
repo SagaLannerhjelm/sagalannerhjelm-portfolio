494633cf1edccf5a76b7188c16815f41a8400e52
Saga Lannerhjelm <lasa21ui@student.ju.se>
2022-09-25 17:18:56 +0200

    Database in use and CRUD on all reasources

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const expressHandlebars = require("express-handlebars");
-// const data = require("./data");
 const sqlite3 = require("sqlite3");
+const bodyParser = require("body-parser");
+const expressSession = require("express-session");
 
 const db = new sqlite3.Database("portfolio-database.db");
+
+const adminMail = "ALice.gmail";
+const adminPassword = "123";
 
 db.run(`
   CREATE TABLE IF NOT EXISTS projects (
@@ -28,7 +32,7 @@ db.run(`
 
 db.run(`
   CREATE TABLE IF NOT EXISTS comments (
-    cmnttId INTEGER PRIMARY KEY,
+    cmntId INTEGER PRIMARY KEY,
     cmntName TEXT,
     cmntDate TEXT,
     cmntContent TEXT,
@@ -47,12 +51,27 @@ db.run(`
 `);
 
 const app = express();
+expressHandlebars.register;
+
+// Following code was made with help by Esterling Accime's video https://www.youtube.com/watch?v=2BoSBaWvFhM, retrieved 2022-09-24
+const hbs = expressHandlebars.create({
+  defaultLayout: "main.hbs",
+
+  // Create custom helper
+  helpers: {
+    // Following function retrieved from https://stackoverflow.com/questions/34252817/handlebarsjs-check-if-a-string-is-equal-to-a-value 2022-09-24
+    ifEquals: function (arg1, arg2, options) {
+      return arg1 == arg2 ? options.fn(this) : options.inverse(this);
+    },
+  },
+});
 
 app.engine(
   "hbs",
-  expressHandlebars.engine({
-    defaultLayout: "main.hbs",
-  })
+  hbs.engine
+  // expressHandlebars.engine({
+  //   defaultLayout: "main.hbs",
+  // })
 );
 
 app.use(express.static("public"));
@@ -63,17 +82,58 @@ app.use(
   })
 );
 
+// app.use(
+//   bodyParser.urlencoded({
+//     extended: false,
+//   })
+// );
+
+//Midleware function to handle expressSession
+// app.use(
+//   expressSession({
+//     saveUninitalized: false,
+//     resave: false,
+//     secret: "aiuhkslao",
+//   })
+// );
+
 app.get("/", function (request, response) {
-  const query = `SELECT * FROM projects`;
+  const query = `SELECT * FROM projects ORDER BY projId DESC`;
 
   db.all(query, function (error, projects) {
+    const errorMessage = [];
+
+    if (error) {
+      errorMessage.push("Inernal server error");
+    }
+
     const model = {
+      errorMessage,
       projects,
     };
     response.render("startpage.hbs", model);
   });
 });
 
+// app.get("/projects/illustrations", function (request, response) {
+//   const query = `SELECT * FROM projects WHERE projCategory = "Website" ORDER BY projId DESC`;
+
+//   db.all(query, function (error, projects) {
+//     const errorMessage = [];
+
+//     if (error) {
+//       errorMessage.push("Inernal server error");
+//     }
+
+//     const model = {
+//       errorMessage,
+//       projects,
+//     };
+//     response.render("startpage.hbs", model);
+//   });
+// });
+
+// Read projects with a specific id and render corresponding page
 app.get("/projects/:id", function (request, response) {
   const id = request.params.id;
 
@@ -88,38 +148,55 @@ app.get("/projects/:id", function (request, response) {
   });
 });
 
+// Renders blog page
 app.get("/blog", function (request, response) {
-  const query = `SELECT * FROM blogposts`;
+  const blogQuery = `SELECT * FROM blogposts ORDER BY blogId DESC`;
+  const commentsQuery = `SELECT * FROM comments`;
 
-  db.all(query, function (error, blogposts) {
-    const model = {
-      blogposts,
-      // comments: data.comments,
-    };
-    response.render("blog.hbs", model);
+  db.all(blogQuery, function (error, blogposts) {
+    db.all(commentsQuery, function (error, comments) {
+      const model = {
+        blogposts,
+        comments,
+      };
+      response.render("blog.hbs", model);
+    });
   });
 });
 
+//An attempt to put blogposts and comments on the same page
+// app.get("/blog", function (request, response) {
+//   const query = `SELECT blogposts.*, comments.* FROM blogposts LEFT OUTER JOIN comments ON blogposts.blogId = comments.blogId`;
+//   db.all(query, function (error, blogposts, hello) {
+//     const model = {
+//       blogposts,
+//       hello,
+//     };
+//     response.render("blog.hbs", model);
+//   });
+// });
+
+// Creates new comments on blog page
 app.post("/blog/:id", function (request, response) {
   const blogId = request.params.id;
 
   const name = request.body.name;
   const comment = request.body.comment;
+
+  // Calculate today's date
   const today = new Date();
   const year = today.getFullYear();
   const month = today.getMonth();
-  const monthCorrection = month.length <= 1 ? "0" + month + 1 : month + 1;
+  const monthCorrection = month < 10 ? "0" + (month + 1) : month + 1;
   const date = today.getDate();
-  const time = year + "-" + monthCorrection + "-" + date;
+  const currentDate = year + "-" + monthCorrection + "-" + date;
 
-  data.comments.push({
-    id: data.comments.at(-1).id + 1,
-    name: name,
-    content: comment,
-    date: time,
-    blogId: blogId,
+  const query = `INSERT INTO comments (cmntName, cmntDate, cmntContent, blogId) VALUES (?, ?, ?, ?)`;
+  const values = [name, currentDate, comment, blogId];
+
+  db.get(query, values, function (error) {
+    response.redirect("/blog");
   });
-  response.redirect("/blog");
 });
 
 app.get("/about", function (request, response) {
@@ -132,6 +209,37 @@ app.get("/contact", function (request, response) {
 
 app.get("/login", function (request, response) {
   response.render("login.hbs");
+});
+
+// Example
+
+// app.get("/secret-page", function () {
+//   if (request.session.isLoggedIn === true) {
+//     //send back secret page
+//   } else {
+//     //send back error
+//   }
+// });
+
+let isLoggedIn = false;
+
+app.post("/login", function (request, response) {
+  const mail = request.body.mail;
+  const password = request.body.password;
+
+  if (mail == adminMail && password == adminPassword) {
+    request.session.isLoggedIn = true;
+
+    response.redirect("/");
+  } else {
+    const model = {
+      failedToLogin: false,
+    };
+
+    response.render("login.hbs");
+  }
+
+  response.redirect("account.hbs");
 });
 
 app.get("/account", function (request, response) {
@@ -157,39 +265,54 @@ app.post("/new-project", function (request, response) {
   const category = request.body.category;
   const file = request.body.file;
 
-  data.projects.unshift({
-    id: data.projects.at(0).id + 1,
-    Title: title,
-    Description: description,
-    Date: date,
-    Category: category,
-    Picture: file,
-  });
+  const errorMessage = [];
 
-  response.redirect("/projects/" + data.projects.length);
+  const titleMaxLength = 100;
+  const descriptionMaxLenght = 1000;
+
+  // validation for title
+  if (title === "") {
+    errorMessage.push("Title can't be empty");
+  } else if (title.length > titleMaxLength) {
+    errorMessage.push("Title is more than " + titleMaxLength + " characters");
+  }
+
+  if (description === "") {
+    errorMessage.push("Description can't be empty");
+  } else if (title.length > descriptionMaxLenght) {
+    errorMessage.push(
+      "Title is more than " + descriptionMaxLenght + " characters"
+    );
+  }
+
+  if (errorMessage.length === 0) {
+    const query = `INSERT INTO projects (projTitle, projDescription, projDate, projCategory, projPicture) VALUES (?, ?, ?, ?, ?)`;
+
+    const values = [title, description, date, category, file];
+
+    db.run(query, values, function (error) {
+      if (error) {
+        errorMessage.push("Internal server error");
+
+        const model = {
+          errorMessage,
+        };
+
+        response.render("new-project.hbs", model);
+      } else {
+        response.redirect("/#projects");
+      }
+    });
+  } else {
+    const model = {
+      errorMessage,
+    };
+
+    response.render("new-project.hbs", model);
+  }
 });
 
-app.get("/new-blog", function (request, response) {
-  response.render("new-blogpost.hbs");
-});
-
-app.post("/new-blog", function (request, response) {
-  const title = request.body.title;
-  const description = request.body.description;
-  const date = new Date();
-  const file = request.body.file;
-
-  data.blogposts.unshift({
-    id: data.blogposts.at(0).id + 1,
-    Title: title,
-    Description: description,
-    Date: date,
-    Picture: file,
-  });
-
-  response.redirect("/blog");
-});
-
+// Renders the edit page for a project
 app.get("/edit-project/:id", function (request, response) {
   const id = request.params.id;
 
@@ -204,6 +327,72 @@ app.get("/edit-project/:id", function (request, response) {
   });
 });
 
+// Edits a project with a specific id
+app.post("/edit-project/:id", function (request, response) {
+  const id = request.params.id;
+
+  const title = request.body.title;
+  const description = request.body.description;
+  const date = request.body.date;
+  const category = request.body.category;
+  const file = request.body.file;
+
+  const query = `UPDATE projects SET 
+                projTitle = ?, 
+                projDescription = ?, 
+                projDate = ?, 
+                projCategory = ?, 
+                projPicture = ? 
+                WHERE projId = ?`;
+
+  const values = [title, description, date, category, file, id];
+
+  db.run(query, values, function (error) {
+    response.redirect("/#projects");
+  });
+});
+
+// Deletes a project with a specific id
+app.post("/projects/:id", function (request, response) {
+  const id = request.params.id;
+
+  const query = `DELETE FROM projects WHERE projId = ?`;
+
+  const values = [id];
+
+  db.run(query, values, function (error) {
+    response.redirect("/#projects");
+  });
+});
+
+// Renders the page for creating a new blog post
+app.get("/new-blog", function (request, response) {
+  response.render("new-blogpost.hbs");
+});
+
+app.post("/new-blog", function (request, response) {
+  const title = request.body.title;
+  const description = request.body.description;
+
+  // Calculate today's date
+  const today = new Date();
+  const year = today.getFullYear();
+  const month = today.getMonth();
+  const monthCorrection = month < 10 ? "0" + (month + 1) : month + 1;
+  const date = today.getDate();
+  const currentDate = year + "-" + monthCorrection + "-" + date;
+
+  const file = request.body.file;
+
+  const query = `INSERT INTO blogposts (blogTitle, blogDescription, blogDate, blogPicture) VALUES (?, ?, ?, ?)`;
+  const values = [title, description, currentDate, file];
+
+  db.run(query, values, function (error) {
+    response.redirect("/blog");
+  });
+});
+
+// Shows the edit page for a blog post
 app.get("/edit-blog/:id", function (request, response) {
   const id = request.params.id;
 
@@ -215,6 +404,81 @@ app.get("/edit-blog/:id", function (request, response) {
       blog,
     };
     response.render("edit-blogpost.hbs", model);
+  });
+});
+
+// Edits a blog post with a specific id
+app.post("/edit-blog/:id", function (request, response) {
+  const id = request.params.id;
+
+  const title = request.body.title;
+  const description = request.body.description;
+
+  const file = request.body.file;
+
+  const query = `UPDATE blogposts SET blogTitle = ?, blogDescription = ?, blogPicture = ? WHERE blogId = ?`;
+
+  const values = [title, description, file, id];
+
+  db.run(query, values, function (error) {
+    response.redirect("/blog");
+  });
+});
+
+// Deletes a blog with a specific id
+app.post("/delete-blog/:id", function (request, response) {
+  const id = request.params.id;
+
+  const query = `DELETE FROM blogposts WHERE blogId = ?`;
+
+  const values = [id];
+
+  db.run(query, values, function (error) {
+    response.redirect("/blog");
+  });
+});
+
+// Edit comment page
+app.get("/edit-comment/:id", function (request, response) {
+  const id = request.params.id;
+
+  const query = `SELECT * FROM comments WHERE cmntId = ?`;
+  const values = [id];
+
+  db.get(query, values, function (error, comment) {
+    const model = {
+      comment,
+    };
+    response.render("edit-comment.hbs", model);
+  });
+});
+
+// Edits a project with a specific id
+app.post("/edit-comment/:id", function (request, response) {
+  const id = request.params.id;
+
+  const name = request.body.name;
+  const comment = request.body.comment;
+
+  const query = `UPDATE comments SET cmntName = ?, cmntContent = ? WHERE cmntId = ?`;
+
+  const values = [name, comment, id];
+
+  db.run(query, values, function (error) {
+    response.redirect("/blog");
+  });
+});
+
+// Deletes a comment with a specific id
+app.post("/delete-comment/:id", function (request, response) {
+  const id = request.params.id;
+
+  const query = `DELETE FROM comments WHERE cmntId = ?`;
+
+  const values = [id];
+
+  db.run(query, values, function (error) {
+    response.redirect("/blog");
   });
 });
 
