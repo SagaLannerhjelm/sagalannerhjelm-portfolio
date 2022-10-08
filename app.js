90bfec812eeeba814fd3ff2283d6eb193651a2bf
Saga Lannerhjelm <lasa21ui@student.ju.se>
2022-10-08 17:07:12 +0200

    Remove picture from file system

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const connectSqLite3 = require("connect-sqlite3");
 const SQLiteStore = connectSqLite3(expressSession);
 const fileUpload = require("express-fileupload");
 const bcrypt = require("bcrypt");
+const fs = require("fs");
 
 // Variables
 const titleMaxLength = 40;
@@ -300,6 +301,42 @@ app.post("/blog/:id", function (request, response) {
   }
 });
 
+app.get("/new-blog-picture/:id", function (request, response) {
+  const id = request.params.id;
+
+  const query = `SELECT * FROM blogposts WHERE blogId = ?`;
+  const values = [id];
+
+  db.get(query, values, function (error, blog) {
+    const errorMessages = [];
+    if (error) {
+      errorMessages.push("Internal server error");
+    }
+    const model = {
+      blog,
+    };
+    response.render("new-blog-picture.hbs", model);
+  });
+});
+
+app.get("/edit-blog-picture/:id", function (request, response) {
+  const id = request.params.id;
+
+  const query = `SELECT * FROM blogposts WHERE blogId = ?`;
+  const values = [id];
+
+  db.get(query, values, function (error, blog) {
+    const errorMessages = [];
+    if (error) {
+      errorMessages.push("Internal server error");
+    }
+    const model = {
+      blog,
+    };
+    response.render("edit-blog-picture.hbs", model);
+  });
+});
+
 app.get("/about", function (request, response) {
   response.render("about.hbs");
 });
@@ -382,64 +419,122 @@ app.post("/logout", function (request, response) {
 });
 
 // Following tutorial by Raddy on youtube: https://www.youtube.com/watch?v=hyJiNTFtQic date: 2022-10-06
-app.post("/upload-picture/:id", function (request, response) {
-  const id = request.params.id;
+app.post(
+  "/upload-picture/:reason/:destination/:id",
+  function (request, response) {
+    const reason = request.params.reason;
+    const destination = request.params.destination;
+    const id = request.params.id;
 
-  let imageFile;
-  let uploadPath;
+    let imageFile;
+    let uploadPath;
 
-  const errorMessages = [];
+    const errorMessages = [];
 
-  if (!request.files || Object.keys(request.files).length === 0) {
-    // return response.status(400).send("No files were uploaded.");
-    errorMessages.push("No file is selected");
-    const model = {
-      errorMessages,
-      project: {
-        projId: id,
-      },
-    };
-    response.render("new-project-picture.hbs", model);
-  } else {
-    // imageFile is the name of the input
-    imageFile = request.files.image;
-    // Give each file name some random numbers infront of them to make them unique
-    const uniqueFileName = Math.floor(Math.random() * 10000) + imageFile.name;
+    if (!request.files || Object.keys(request.files).length === 0) {
+      // return response.status(400).send("No files were uploaded.");
+      errorMessages.push("No file is selected");
 
-    uploadPath = __dirname + "/public/uploads/" + uniqueFileName;
+      let model;
 
-    // Move the uploaded file to the right place
-    imageFile.mv(uploadPath, function (error) {
-      if (error) {
-        return response.status(500).send(error);
-      }
-
-      if (errorMessages.length === 0) {
-        const query = `UPDATE projects SET projPicture = ? WHERE projId = ?`;
-
-        const values = [uniqueFileName, id];
-
-        db.run(query, values, function (error) {
-          if (error) {
-            errorMessages.push("Internal server error");
-            const model = {
-              errorMessages,
-            };
-            response.render("new-project-picture.hbs", model);
-          } else {
-            response.redirect("/#projects");
-          }
-        });
-      } else {
-        const model = {
+      if (destination === "project") {
+        model = {
           errorMessages,
+          project: {
+            projId: id,
+          },
         };
-
-        response.render("new-project-picture.hbs", model);
+      } else if (destination === "blog") {
+        model = {
+          errorMessages,
+          blog: {
+            blogId: id,
+          },
+        };
       }
-    });
+      // Render the same page when error occured
+      if (reason === "new" && destination === "project") {
+        response.render("new-project-picture.hbs", model);
+      } else if (reason === "edit" && destination === "project") {
+        response.render("edit-project-picture.hbs", model);
+      } else if (reason === "new" && destination === "blog") {
+        response.render("new-blog-picture.hbs", model);
+      } else if (reason === "edit" && destination === "blog") {
+        response.render("edit-blog-picture.hbs", model);
+      }
+    } else {
+      // imageFile is the name of the input
+      imageFile = request.files.image;
+      // Give each file name some random numbers infront of them to make them unique
+      const uniqueFileName = Math.floor(Math.random() * 10000) + imageFile.name;
+
+      uploadPath = __dirname + "/public/uploads/" + uniqueFileName;
+
+      // Move the uploaded file to the right place
+      imageFile.mv(uploadPath, function (error) {
+        if (error) {
+          return response.status(500).send(error);
+        }
+
+        if (errorMessages.length === 0) {
+          let query;
+
+          if (destination === "project") {
+            query = `UPDATE projects SET projPicture = ? WHERE projId = ?`;
+          } else if (destination === "blog") {
+            query = `UPDATE blogposts SET blogPicture = ? WHERE blogId = ?`;
+          }
+
+          const values = [uniqueFileName, id];
+
+          db.run(query, values, function (error) {
+            if (error) {
+              errorMessages.push("Internal server error");
+              const model = {
+                errorMessages,
+              };
+              // Render the same page when error occured
+              if (reason === "new" && destination === "project") {
+                response.render("new-project-picture.hbs", model);
+              } else if (reason === "edit" && destination === "project") {
+                response.render("edit-project-picture.hbs", model);
+              } else if (reason === "new" && destination === "blog") {
+                response.render("new-blog-picture.hbs", model);
+              } else if (reason === "edit" && destination === "blog") {
+                response.render("edit-blog-picture.hbs", model);
+              }
+            } else {
+              // Redirect to another page when upload is completed
+              if (reason === "new" && destination === "project") {
+                response.redirect("/#projects");
+              } else if (reason === "edit" && destination === "project") {
+                response.redirect("/edit-project/" + id);
+              } else if (reason === "new" && destination === "blog") {
+                response.redirect("/blog");
+              } else if (reason === "edit" && destination === "blog") {
+                response.redirect("/edit-blog/" + id);
+              }
+            }
+          });
+        } else {
+          const model = {
+            errorMessages,
+          };
+          // Render the same page when error occured
+          if (reason === "new" && destination === "project") {
+            response.render("new-project-picture.hbs", model);
+          } else if (reason === "edit" && destination === "project") {
+            response.render("edit-project-picture.hbs", model);
+          } else if (reason === "new" && destination === "blog") {
+            response.render("new-blog-picture.hbs", model);
+          } else if (reason === "edit" && destination === "blog") {
+            response.render("edit-blog-picture.hbs", model);
+          }
+        }
+      });
+    }
   }
-});
+);
 
 app.get("/new-project", function (request, response) {
   response.render("new-project.hbs");
@@ -450,7 +545,6 @@ app.post("/new-project", function (request, response) {
   const description = request.body.description;
   const date = request.body.date;
   const category = request.body.category;
-  // const file = request.body.file;
 
   const errorMessages = [];
 
@@ -487,11 +581,6 @@ app.post("/new-project", function (request, response) {
     if (date === "") {
       errorMessages.push("Pick a date");
     }
-
-    // validation for picture
-    // if (file === "") {
-    //   errorMessages.push("Choose a picture");
-    // }
   }
 
   if (errorMessages.length === 0) {
@@ -514,6 +603,10 @@ app.post("/new-project", function (request, response) {
   } else {
     const model = {
       errorMessages,
+      title,
+      description,
+      date,
+      category,
     };
 
     response.render("new-project.hbs", model);
@@ -637,6 +730,10 @@ app.post("/edit-project/:id", function (request, response) {
       const model = {
         errorMessages,
         project,
+        title,
+        description,
+        date,
+        category,
       };
       response.render("edit-project.hbs", model);
     });
@@ -661,93 +758,57 @@ app.get("/edit-project/picture/:id", function (request, response) {
   });
 });
 
-app.post("/edit-project/picture/:id", function (request, response) {
-  const id = request.params.id;
-
-  let imageFile;
-  let uploadPath;
-
-  const errorMessages = [];
-
-  if (!request.files || Object.keys(request.files).length === 0) {
-    errorMessages.push("No file is selected");
-    const model = {
-      errorMessages,
-      project: {
-        projId: id,
-      },
-    };
-    response.render("new-project-picture.hbs", model);
-  } else {
-    // imageFile is the name of the input
-    imageFile = request.files.image;
-    // Give each file name some random numbers infront of them to make them unique
-    const uniqueFileName = Math.floor(Math.random() * 10000) + imageFile.name;
-
-    uploadPath = __dirname + "/public/uploads/" + uniqueFileName;
-
-    // Move the uploaded file to the right place
-    imageFile.mv(uploadPath, function (error) {
-      if (error) {
-        return response.status(500).send(error);
-      }
-
-      if (errorMessages.length === 0) {
-        const query = `UPDATE projects SET projPicture = ? WHERE projId = ?`;
-
-        const values = [uniqueFileName, id];
-
-        db.run(query, values, function (error) {
-          if (error) {
-            errorMessages.push("Internal server error");
-            const model = {
-              errorMessages,
-            };
-            response.render("edit-project-picture.hbs", model);
-          } else {
-            response.redirect("/edit-project/" + id);
-          }
-        });
-      } else {
-        const model = {
-          errorMessages,
-        };
-
-        response.render("edit-project-picture.hbs", model);
-      }
-    });
-  }
-});
-
 // Deletes a project with a specific id
 app.post("/project/:id", function (request, response) {
   const id = request.params.id;
 
-  const query = `DELETE FROM projects WHERE projId = ?`;
+  const errorMessages = [];
 
+  // Get file name of image
+  const imgUrlQuery = `SELECT projPicture FROM projects WHERE projId = ?`;
   const values = [id];
 
-  db.run(query, values, function (error) {
-    const errorMessages = [];
+  db.get(imgUrlQuery, values, function (error, project) {
     if (error) {
       errorMessages.push("Internal server error");
+    }
+    const pictureFileName = project.projPicture;
 
-      const query = `SELECT * FROM projects WHERE projId = ?`;
+    // Try to delete the file form the file system
+    fs.unlink("public/uploads/" + pictureFileName, function (error) {
+      if (error) {
+        errorMessages.push(
+          "Problem occured when deleting picture from file system"
+        );
+      }
+
+      // If no error with deleting form filesystem, then delete from database
+      const query = `DELETE FROM projects WHERE projId = ?`;
       const values = [id];
 
-      db.get(query, values, function (error, project) {
+      db.run(query, values, function (error) {
         if (error) {
           errorMessages.push("Internal server error");
+
+          const query = `SELECT * FROM projects WHERE projId = ?`;
+          const values = [id];
+
+          db.get(query, values, function (error, project) {
+            if (error) {
+              errorMessages.push("Internal server error");
+            }
+            const model = {
+              project,
+              errorMessages,
+            };
+            response.render("project-detail.hbs", model);
+          });
+        } else {
+          response.redirect("/#projects");
         }
-        const model = {
-          project,
-          errorMessages,
-        };
-        response.render("project-detail.hbs", model);
       });
-    } else {
-      response.redirect("/#projects");
-    }
+      console.log("File is deleted.");
+    });
   });
 });
 
@@ -768,8 +829,6 @@ app.post("/new-blog", function (request, response) {
   const date = today.getDate();
   const dateCorrection = date < 10 ? "0" + date : date;
   const currentDate = year + "-" + monthCorrection + "-" + dateCorrection;
-
-  const file = request.body.file;
 
   const errorMessages = [];
 
@@ -794,16 +853,11 @@ app.post("/new-blog", function (request, response) {
         "Title is more than " + descriptionMaxLenght + " characters"
       );
     }
-
-    // Validation for picture
-    if (file === "") {
-      errorMessages.push("Choose a picture");
-    }
   }
 
   if (errorMessages.length === 0) {
-    const query = `INSERT INTO blogposts (blogTitle, blogDescription, blogDate, blogPicture) VALUES (?, ?, ?, ?)`;
-    const values = [title, description, currentDate, file];
+    const query = `INSERT INTO blogposts (blogTitle, blogDescription, blogDate) VALUES (?, ?, ?)`;
+    const values = [title, description, currentDate];
 
     db.run(query, values, function (error) {
       if (error) {
@@ -813,12 +867,14 @@ app.post("/new-blog", function (request, response) {
         };
         response.render("new-blogpost.hbs", model);
       } else {
-        response.redirect("/blog");
+        response.redirect("/new-blog-picture/" + this.lastID);
       }
     });
   } else {
     const model = {
       errorMessages,
+      title,
+      description,
     };
     response.render("new-blogpost.hbs", model);
   }
@@ -851,7 +907,6 @@ app.post("/edit-blog/:id", function (request, response) {
 
   const title = request.body.title;
   const description = request.body.description;
-  const file = request.body.file;
 
   const errorMessages = [];
 
@@ -876,17 +931,12 @@ app.post("/edit-blog/:id", function (request, response) {
         "Title is more than " + descriptionMaxLenght + " characters"
       );
     }
-
-    // Validation for picture
-    if (file === "") {
-      errorMessages.push("Choose a picture");
-    }
   }
 
   if (errorMessages.length === 0) {
-    const query = `UPDATE blogposts SET blogTitle = ?, blogDescription = ?, blogPicture = ? WHERE blogId = ?`;
+    const query = `UPDATE blogposts SET blogTitle = ?, blogDescription = ? WHERE blogId = ?`;
 
-    const values = [title, description, file, id];
+    const values = [title, description, id];
 
     db.run(query, values, function (error) {
       if (error) {
@@ -897,7 +947,6 @@ app.post("/edit-blog/:id", function (request, response) {
             blogId: id,
             title,
             description,
-            file,
           },
         };
         response.render("edit-blogpost.hbs", model);
@@ -916,54 +965,84 @@ app.post("/edit-blog/:id", function (request, response) {
       const model = {
         errorMessages,
         blog,
+        title,
+        description,
       };
       response.render("edit-blogpost.hbs", model);
     });
   }
 });
 
+db.all(`SELECT COUNT(*) FROM blogposts`, function (error, blogposts) {
+  const hello = blogposts.blogposts;
+  console.log(hello);
+});
+
 // Deletes a blog with a specific id
 app.post("/delete-blog/:id", function (request, response) {
   const id = request.params.id;
 
-  const blogpostQuery = `DELETE FROM blogposts WHERE blogId = ?`;
-  const commentQuery = `DELETE FROM comments WHERE blogId = ?`;
+  const errorMessages = [];
 
+  // Get file name of image
+  const imgUrlQuery = `SELECT blogPicture FROM blogposts WHERE blogId = ?`;
   const values = [id];
 
-  db.run(blogpostQuery, values, function (error) {
-    const serverErrorMessages = [];
+  db.get(imgUrlQuery, values, function (error, blogpost) {
     if (error) {
-      serverErrorMessages.push("Internal server error when deleting blogpost");
+      errorMessages.push("Internal server error");
     }
-    db.run(commentQuery, values, function (error) {
+    const pictureFileName = blogpost.blogPicture;
+
+    // Try to delete the file form the file system
+    fs.unlink("public/uploads/" + pictureFileName, function (error) {
       if (error) {
-        serverErrorMessages.push(
-          "Internal server error when deleting comments connected to blogpost"
+        errorMessages.push(
+          "Problem occured when deleting picture from file system"
         );
-
-        const blogQuery = `SELECT * FROM blogposts ORDER BY blogId DESC`;
-        const commentsQuery = `SELECT * FROM comments`;
-
-        db.all(blogQuery, function (error, blogposts) {
-          if (error) {
-            serverErrorMessages.push("Internal server error");
-          }
-          db.all(commentsQuery, function (error, comments) {
-            if (error) {
-              serverErrorMessages.push("Internal server error");
-            }
-            const model = {
-              blogposts,
-              comments,
-              serverErrorMessages,
-            };
-            response.render("blog.hbs", model);
-          });
-        });
-      } else {
-        response.redirect("/blog");
       }
+
+      // If no error with deleting form filesystem, then delete from database
+      const blogpostQuery = `DELETE FROM blogposts WHERE blogId = ?`;
+      const commentQuery = `DELETE FROM comments WHERE blogId = ?`;
+
+      db.run(blogpostQuery, values, function (error) {
+        const serverErrorMessages = [];
+        if (error) {
+          serverErrorMessages.push(
+            "Internal server error when deleting blogpost"
+          );
+        }
+        db.run(commentQuery, values, function (error) {
+          if (error) {
+            serverErrorMessages.push(
+              "Internal server error when deleting comments connected to blogpost"
+            );
+
+            const blogQuery = `SELECT * FROM blogposts ORDER BY blogId DESC`;
+            const commentsQuery = `SELECT * FROM comments`;
+
+            db.all(blogQuery, function (error, blogposts) {
+              if (error) {
+                serverErrorMessages.push("Internal server error");
+              }
+              db.all(commentsQuery, function (error, comments) {
+                if (error) {
+                  serverErrorMessages.push("Internal server error");
+                }
+                const model = {
+                  blogposts,
+                  comments,
+                  serverErrorMessages,
+                };
+                response.render("blog.hbs", model);
+              });
+            });
+          } else {
+            response.redirect("/blog");
+          }
+        });
+      });
     });
   });
 });
@@ -1061,6 +1140,7 @@ app.post("/edit-comment/:id", function (request, response) {
 // Deletes a comment with a specific id
 app.post("/delete-comment/:id", function (request, response) {
   const id = request.params.id;
+  const blogId = request.body.blogId;
 
   const errorMessages = [];
   // Check if user is logged in
@@ -1076,7 +1156,7 @@ app.post("/delete-comment/:id", function (request, response) {
       if (error) {
         errorMessages.push("Internal server error");
       }
-      response.redirect("/blog");
+      response.redirect("/blog/#blog/" + blogId);
     });
   }
 });
